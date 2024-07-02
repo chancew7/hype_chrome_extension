256acc389644c6b4d44af05694dc56c1785c3981
Chancew7 <cwoosley31@gmail.com>
2024-07-02 12:53:29 -0400

    added pictures and messages and random selection

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,1 +1,39 @@
-console.log("This is a popup!")
+
+list_length = 3;
+const randomIndex = Math.floor(Math.random() * list_length);
+
+
+const imageUrls = [];
+const messageUrls = [];
+
+for (let i = 1; i <= list_length; i++){
+    imageUrls.push("/pictures/rand_pictures/p"+i+".png");
+    messageUrls.push("/messages/m"+i+".txt");
+}
+
+async function fetchMessage(url){
+    const response = await fetch(url);
+    const text = await response.text();
+    return text;
+}
+
+function generateRandomImages(){
+    document.getElementById('random-image').src = imageUrls[randomIndex];
+}
+
+async function generateRandomMessages(){
+    const message = await fetchMessage(messageUrls[randomIndex]);
+    document.getElementById('random-message').innerText = message;
+}
+
+async function generateRandomContent(){
+    generateRandomImages();
+    await generateRandomMessages();
+}
+
+generateRandomContent();
+
+
+
+
+
